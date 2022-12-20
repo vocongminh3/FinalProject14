a213e49b5514823642158f57fedff1d04285b05f
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-12-20 16:53:53 +0700

    update connect db

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const express = require('express')
+import express from 'express';
+import cors from 'cors';
+
+
+import userRouter from './routes/user.route.js';
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(express.json());
+app.use(cors(
+    {
+        origin: '*',
+        methods: 'GET,PATCH,POST,DELETE'
+    }
+));
+
+
+app.use('/api/users', userRouter);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,12 @@ export default knex ({
     client: 'pg',
     connection: {
       host : 'dpg-cdmhcg4gqg43l6fo2oog-a.oregon-postgres.render.com',
+      ssl: true,
       port : 5432,
       user : 'finalproject14',
       password : 'qFL0oZgd4XTi6zKYS12wIIYjTEv8v7qS',
       database : 'finalproject14'
+      
     },
 
     pool: { min: 0, max: 10 }
