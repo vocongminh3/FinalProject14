f9baee6e99bfe14bcc556a3455c8aad7ffd54cf3
Nguyễn Bá Việt <51191112+Nguyenbaviet112@users.noreply.github.com>
2022-12-29 22:12:00 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 
 
 import userRouter from './routes/user.route.js';
+import auth from './middle_wares/auth.mdw.js'
+import authRouter from './routes/auth.route.js';
 
 const app = express()
 const port = 3000
@@ -21,7 +23,7 @@ app.use(cors(
 
 
 app.use('/api/users', userRouter);
-
+app.use('/api/auth', authRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
